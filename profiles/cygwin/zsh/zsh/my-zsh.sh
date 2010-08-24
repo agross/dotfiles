b640d99e35353f93cf112c512448197399994609
Alexander Groß <agross@therightstuff.de>
2010-08-25 00:25:20 +0200

    Re-adding oh-my-zsh's plugin concept to use git-flow's completion for zsh

--- profiles/cygwin/zsh/zsh/my-zsh.sh
+++ profiles/cygwin/zsh/zsh/my-zsh.sh
@@ -9,3 +9,7 @@ typeset -U fpath
 
 # Load all of the config files in ~/.zsh/lib that end in .zsh
 for config_file ($ZSH/lib/*.zsh) source $config_file
+
+# Load all of the plugins that were defined in ~/.zshrc
+plugin=${plugin:=()}
+for plugin ($plugins) source $ZSH/plugins/$plugin.plugin.zsh
