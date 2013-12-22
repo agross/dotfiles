41251b7c2f244976fa605456d56205badafab6ba
Alexander Groß <agross@therightstuff.de>
2013-12-22 13:38:15 +0100

    Adding WinMerge diff and merge tool

---
+++ profiles/cygwin/bin/bin/git/tools/winmerge-diff.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+path="$(cygpath $1)"
+old="$(cygpath --mixed --absolute "$1")"
+oldUnixFormat="$(cygpath --unix --absolute "$1")"
+new="$(cygpath --mixed --absolute "$2")"
+newUnixFormat="$(cygpath --unix --absolute "$2")"
+tool="$(cygpath --unix --absolute "C:/Tools/WinMerge/WinMergeU.exe")"
+
+title1="Old"
+title2="New $path"
+
+if [ ! -f "$oldUnixFormat" ]
+then
+	old="$(cygpath --mixed --absolute ~/bin/git/tools/empty)"
+	title1="<File didn't exist>"
+fi
+
+if [ ! -f "$newUnixFormat" ]
+then
+	new="$(cygpath --mixed --absolute ~/bin/git/tools/empty)"
+	title1="Old $path"
+	title2="<File has been deleted>"
+fi
+
+#echo -e "path\n$path"
+#echo -e "old\n$old"
+#echo -e "new\n$new"
+
+"$tool" /e /u /dl "$title1" /dr "$title2" "$old" "$new"
