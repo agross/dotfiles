66bcf9b3920751818d9a9a68a2e8ffdaff166424
Alexander Groß <agross@therightstuff.de>
2013-12-22 13:35:20 +0100

    Removing unnecessary lines from meld-diff script

--- profiles/cygwin/bin/bin/git/tools/meld-diff.sh
+++ profiles/cygwin/bin/bin/git/tools/meld-diff.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
 
 old="$(cygpath --mixed --absolute "$1")"
-oldUnixFormat="$(cygpath --unix --absolute "$1")"
-
 new="$(cygpath --mixed --absolute "$2")"
-newUnixFormat="$(cygpath --unix --absolute "$2")"
 
 tool="$(cygpath --unix --absolute "C:/Tools/Meld/meld/meld.exe")"
 
