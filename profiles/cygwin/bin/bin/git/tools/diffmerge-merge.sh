15a84177a487e935e286341f6d5e2c0647c3069b
Alexander Groß <agross@therightstuff.de>
2014-01-13 18:05:53 +0100

    Fixed DiffMerge invocation

--- profiles/cygwin/bin/bin/git/tools/diffmerge-merge.sh
+++ profiles/cygwin/bin/bin/git/tools/diffmerge-merge.sh
@@ -6,7 +6,7 @@
 script_path=${0%/*}
 source "$script_path/diff-and-merge-support.sh"
 
-difftool="/c/Tools/DiffMerge/diffmerge.exe"
+mergetool="/c/Tools/DiffMerge/diffmerge.exe"
 get_merge_args "$1" "$2" "$3" "$4"
 
 "$mergetool" --merge --result="$result" "$local" "$base" "$remote" --title1="Mine" --title2="Merged: $4" --title3="Theirs"
