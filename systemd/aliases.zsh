f74930b24a89d4422d971a0827e99a751be8e4a7
Alexander Groß <agross@therightstuff.de>
2015-12-23 00:13:39 +0100

    Reformat systemd aliases

--- systemd/aliases.zsh
+++ systemd/aliases.zsh
@@ -1,11 +1,13 @@
-if (( $+commands[systemctl] )); then
-  verbose Setting up $fg[red]systemd$reset_color aliases
-
-  alias sc=systemctl
-  alias scs='systemctl status'
-  alias sca='systemctl start'
-  alias sco='systemctl stop'
-  alias scr='systemctl restart'
-  alias scf='systemctl --failed'
-  alias jf='journalctl --since="1 hour ago" --unit'
+if (( ! $+commands[systemctl] )); then
+  return
 fi
+
+verbose Setting up $fg[red]systemd$reset_color aliases
+
+alias sc=systemctl
+alias scs='systemctl status'
+alias sca='systemctl start'
+alias sco='systemctl stop'
+alias scr='systemctl restart'
+alias scf='systemctl --failed'
+alias jf='journalctl --since="1 hour ago" --unit'
