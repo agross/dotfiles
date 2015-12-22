2cb44e74bed955e91aed074a3df2def438488187
Alexander Groß <agross@therightstuff.de>
2015-12-23 00:13:48 +0100

    Adding etckepper aliases

---
+++ etckeeper/aliases.zsh
@@ -0,0 +1,13 @@
+if (( ! $+commands[etckeeper] )); then
+  return
+fi
+
+verbose Setting up $fg[red]etckeeper$reset_color aliases
+
+alias  ec='sudo etckeeper'
+alias  es='sudo etckeeper vcs status'
+alias  ed='sudo etckeeper vcs diff'
+alias  el='sudo etckeeper vcs log --oneline --decorate --branches --all --graph'
+alias  er='sudo etckeeper vcs log -1 --patch'
+alias eci='sudo etckeeper commit'
+alias edo='sudo etckeeper vcs'
