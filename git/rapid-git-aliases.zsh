f4f50a63422bad698ac15e7cb749926644109b1f
Alexander Groß <agross@therightstuff.de>
2015-12-24 11:53:59 +0100

    Add rapid-git as a faster replacement for git_shizzle

---
+++ git/rapid-git-aliases.zsh
@@ -0,0 +1,15 @@
+# Aliases for file operations.
+alias qs='rapid status'
+alias qa='rapid stage'
+alias qap='rapid stage --patch'
+alias qd='rapid diff'
+alias qdc='rapid diff --staged'
+alias qc='rapid drop'
+alias qu='rapid unstage'
+alias qrm='rapid remove'
+
+# Aliases for branch operations.
+alias qb='rapid branch'
+alias qco='rapid checkout'
+alias qm='rapid merge'
+alias qr='rapid rebase'
