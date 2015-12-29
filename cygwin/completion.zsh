fca4ad97aaf5a6688295bf2689ad5c5b0fcab630
Alexander Groß <agross@therightstuff.de>
2015-12-29 14:17:48 +0100

    Dynamic fake files on Windows

--- cygwin/completion.zsh
+++ cygwin/completion.zsh
@@ -2,8 +2,9 @@ if [[ "$(platform)" != "windows" ]]; then
   return
 fi
 
-# Drives.
-zstyle ':completion:*' fake-files   '/:c' '/:d'
+# Drives that cannot be reached by globbing.
+local drives=($(mount | /usr/bin/grep --perl-regexp '^\w: on /\w ' | cut --delimiter=' ' --fields=3))
+zstyle ':completion:*' fake-files "/:${(j. .)drives//\//}"
 
 # Use kill completion for wkill.
 compdef wkill=kill
