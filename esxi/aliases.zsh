3fd403dca4e0a333818ba8a267bf141de3edf33a
Alexander Groß <agross@therightstuff.de>
2016-01-02 01:05:12 +0100

    Updated scpe alias for ESXi 6.0

--- esxi/aliases.zsh
+++ esxi/aliases.zsh
@@ -1,4 +1,4 @@
 # scp with rsync for ESXi
-# http://blog.magiksys.net/run-rsync-in-server-mode-via-inetd-on-vmware-esxi
+# https://damiendebin.net/blog/2013/12/06/esxi-5-dot-1-and-rsync/
 # Cipher selection: http://blog.famzah.net/2010/06/11/openssh-ciphers-performance-benchmark/
-alias scpe="rsync --partial --progress --rsh='ssh -T -c blowfish-cbc -o Compression=no -x' --rsync-path=/vmfs/volumes/4cae2a76-9a623c13-67f0-002354056c3c/misc/sbin/rsync"
+alias scpe="rsync -vv --partial --progress --rsh='ssh -T -c aes128-ctr -o Compression=no -x' --rsync-path=/vmfs/volumes/4cae2a76-9a623c13-67f0-002354056c3c/bin/rsync"
