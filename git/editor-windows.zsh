9d2f2ab32fa8db5ffea69944daa18e7f8f39f844
Alexander Groß <agross@therightstuff.de>
2016-01-02 17:59:25 +0100

    Don't set git editor on Windows if we're connected using SSH

--- git/editor-windows.zsh
+++ git/editor-windows.zsh
@@ -4,6 +4,11 @@ if [[ "$(platform)" != "windows" ]]; then
   return
 fi
 
+if [[ -n "$SSH_CONNECTION" ]]; then
+  # SSH and Notepad++ won't work.
+  return
+fi
+
 export GIT_EDITOR='file="$(cygpath --windows --absolute "$1")"; shift $#; /c/Tools/Notepad++/notepad++.exe -multiInst -nosession -notabbar -noPlugin "$file"'
 
 # ST opens two windows when there is no instance of ST running.
