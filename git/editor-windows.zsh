be8372f92ab9ecdd91d66eabce8a92e9d72b427f
Alexander Groß <agross@therightstuff.de>
2016-01-02 02:57:49 +0100

    git editor: use Notepad++ on Windows, $EDITOR everywhere else

---
+++ git/editor-windows.zsh
@@ -0,0 +1,10 @@
+# Use Notepad++ as the git editor on Windows.
+
+if [[ "$(platform)" != "windows" ]]; then
+  return
+fi
+
+export GIT_EDITOR='file="$(cygpath --windows --absolute "$1")"; shift $#; /c/Tools/Notepad++/notepad++.exe -multiInst -nosession -notabbar -noPlugin "$file"'
+
+# ST opens two windows when there is no instance of ST running.
+# export GIT_EDITOR='file="$(cygpath --windows --absolute "$1")"; shift $#; /c/Tools/Sublime\ Text/subl.exe --wait --new-window "$file" --'
