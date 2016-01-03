58ab34a3aa54594297c66fd8a7417ea5e8785823
Alexander Groß <agross@therightstuff.de>
2016-01-03 13:59:46 +0100

    Updates ESXi rsync path after creating new RAID-1 volume

--- esxi/aliases.zsh
+++ esxi/aliases.zsh
@@ -1,4 +1,4 @@
 # scp with rsync for ESXi
 # https://damiendebin.net/blog/2013/12/06/esxi-5-dot-1-and-rsync/
 # Cipher selection: http://blog.famzah.net/2010/06/11/openssh-ciphers-performance-benchmark/
-alias scpe="rsync -vv --partial --progress --rsh='ssh -T -c aes128-ctr -o Compression=no -x' --rsync-path=/vmfs/volumes/4cae2a76-9a623c13-67f0-002354056c3c/bin/rsync"
+alias scpe="rsync -vv --partial --progress --rsh='ssh -T -c aes128-ctr -o Compression=no -x' --rsync-path=/vmfs/volumes/568907fe-071195ba-011b-001517f84ff0/bin/rsync"
