a1795c2d20dd3d8f6cfe2c5a6b1743fae054ecc9
Alexander Groß <agross@therightstuff.de>
2016-01-05 21:13:49 +0100

    Disabling SSH agent forwarding on mintty
    It caused problems with the tmp/ssh-agent-$USER-screen symlink pointing to itself

--- ssh/ssh-agent.zsh
+++ ssh/ssh-agent.zsh
@@ -1,1 +1,6 @@
+if is-mintty; then
+  verbose Disabling $fg[red]ssh agent$reset_color forwarding because we\'re running $fg[red]mintty$reset_color
+  return
+fi
+
 zstyle :omz:plugins:ssh-agent agent-forwarding on
