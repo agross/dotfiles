c5e8fe2c612bf400299a2a94dfa608f4af53211d
Alexander Groß <agross@therightstuff.de>
2016-01-05 01:37:49 +0100

    On Windows-export the SSH agent variables such that non-Cygwin programs can use them

---
+++ ssh/postinit.zsh
@@ -0,0 +1,23 @@
+if [[ "$(platform)" != "windows" ]]; then
+  return
+fi
+
+# If we started an SSH agent, make the environment variables known system-wide for use with cmd.exe
+# or Atom's git-plus. Otherwise unset them.
+# Windows Explorer will listen on changes to the global environment variables and new processes
+# started via Explorer will have the new environment variable set.
+if [[ -n "$SSH_AGENT_PID" ]] && [[ -n "$SSH_AUTH_SOCK" ]]; then
+  verbose Exporting system-wide $fg[yellow]\$SSH_AGENT_PID$reset_color as $fg[yellow]$SSH_AGENT_PID$reset_color
+  setx SSH_AGENT_PID "$SSH_AGENT_PID" > /dev/null
+
+  auth_sock="$(cygpath --windows "$SSH_AUTH_SOCK")"
+  verbose Exporting system-wide $fg[yellow]\$SSH_AUTH_SOCK$reset_color as $fg[yellow]$auth_sock$reset_color
+  setx SSH_AUTH_SOCK "$auth_sock" > /dev/null
+  unset auth_sock
+else
+  verbose Removing system-wide export $fg[yellow]\$SSH_AGENT_PID$reset_color
+  setx SSH_AGENT_PID "" > /dev/null
+
+  verbose Removing system-wide export $fg[yellow]\$SSH_AUTH_SOCK$reset_color
+  setx SSH_AUTH_SOCK "" > /dev/null
+fi
