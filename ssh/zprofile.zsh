6d83c8b731504d3dbb4df4bc8a6863bd67dce704
Alexander Groß <agross@therightstuff.de>
2016-01-09 16:21:06 +0100

    Work around robbyrussell/oh-my-zsh#4763

--- ssh/zprofile.zsh
+++ ssh/zprofile.zsh
@@ -1,7 +1,7 @@
 if [[ -n "$SSH_CONNECTION" ]] && (($+commands[screen])); then
   if [[ -n "$SSH_AUTH_SOCK" ]]; then
-    verbose Symlinking $fg[yellow]$SSH_AUTH_SOCK$reset_color to $fg[yellow]/tmp/ssh-agent-$USER-screen$reset_color
-    ln -sf "$SSH_AUTH_SOCK" "/tmp/ssh-agent-$USER-screen"
+    verbose Symlinking $fg[yellow]$SSH_AUTH_SOCK$reset_color to $fg[yellow]$HOME/.ssh/ssh-agent-$USER-$HOSTNAME$reset_color
+    ln -sf "$SSH_AUTH_SOCK" "$HOME/.ssh/ssh-agent-$USER-$HOSTNAME"
   fi
 
   verbose Starting $fg[red]screen$reset_color because we\'re connected using $fg[red]ssh$reset_color
