4c45528776761c318ff069ce7c3fb4f40b75dd63
Alexander Groß <agross@therightstuff.de>
2016-01-24 19:12:33 +0100

    Replace some of my systemd aliases with the systemd plugin from oh-my-zsh

--- systemd/aliases.zsh
+++ systemd/aliases.zsh
@@ -1,13 +1,9 @@
-if (( ! $+commands[systemctl] )); then
+if ((! $+commands[systemctl])); then
   return
 fi
 
 verbose Setting up $fg[red]systemd$reset_color aliases
 
 alias sc=systemctl
-alias scs='systemctl status'
-alias sca='systemctl start'
-alias sco='systemctl stop'
-alias scr='systemctl restart'
 alias scf='systemctl --failed'
 alias jf='journalctl --since="1 hour ago" --unit'
