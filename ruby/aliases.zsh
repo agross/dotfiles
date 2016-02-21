d1846bca226bc9d4e07e0a572d84690773ca8813
Alexander Groß <agross@therightstuff.de>
2016-02-21 15:14:43 +0100

    Adding bundle aliases on Linux

--- ruby/aliases.zsh
+++ ruby/aliases.zsh
@@ -1,13 +1,20 @@
-if [[ "$(platform)" != "windows" ]]; then
+if ((! $+commands[ruby] && ! $+commands[rvm])); then
   return
 fi
 
-verbose Setting up Windows Ruby aliases
+if [[ "$(platform)" == "windows" ]]; then
+  verbose Setting up Windows Ruby aliases
 
-alias gem='gem.bat'
-alias rake='rake.bat'
-alias irb='irb.bat'
-# bundle conflicts with the git-bundle auto-alias, so define a second alias.
-alias bundle='bundle.bat'
-alias bun='nocorrect bundle.bat'
-alias bex='nocorrect bundle.bat exec'
+  alias gem='gem.bat'
+  alias rake='rake.bat'
+  alias irb='irb.bat'
+  # bundle conflicts with the git-bundle auto-alias, so define a second alias.
+  alias bundle='bundle.bat'
+  alias bun='nocorrect bundle.bat'
+  alias bex='nocorrect bundle.bat exec'
+else
+  verbose Setting up Linux Ruby aliases
+
+  alias bun='bundle'
+  alias bex='bundle exec'
+fi
