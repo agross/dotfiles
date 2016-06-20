c41f32810128ba4e2543de13ab0185197dbdbb4e
Alexander Groß <agross@therightstuff.de>
2016-06-20 02:06:02 +0200

    Changed less tabs to 4

--- zsh/lib/exports.zsh
+++ zsh/lib/exports.zsh
@@ -1,5 +1,5 @@
 export PAGER=less
-export LESS='--quit-at-eof --tabs=3 --LONG-PROMPT --quit-if-one-screen --RAW-CONTROL-CHARS --no-init'
+export LESS='--quit-at-eof --tabs=4 --LONG-PROMPT --quit-if-one-screen --RAW-CONTROL-CHARS --no-init'
 
 # Language settings: This variable determines the locale category for any category not specifically selected via a variable starting with `LC_'.
 export LANG=en_US.UTF-8
