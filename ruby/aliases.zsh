dc9c5067bca4f0bb25afe48cfa5e64868f6adb65
Alexander Groß <agross@therightstuff.de>
2016-06-20 10:16:09 +0200

    Make Ruby aliases on Windows dynamic w.r.t. what's installed as a gem

--- ruby/aliases.zsh
+++ ruby/aliases.zsh
@@ -5,11 +5,19 @@ fi
 if [[ "$(platform)" == "windows" ]]; then
   verbose Setting up Windows Ruby aliases
 
-  alias gem='gem.bat'
-  alias rake='rake.bat'
-  alias irb='irb.bat'
-  # bundle conflicts with the git-bundle auto-alias, so define a second alias.
-  alias bundle='bundle.bat'
+  # Define an alias for *.bat file inside the Ruby bin directory.
+  # Windows Ruby installs gem binaries as an extensionless file as well as one
+  # with a .bat extension.
+  # Glob qualifier (N.:r:t):
+  #   N  = ignore no matches
+  #   .  = yield plain files only
+  #   :r = remove file extension
+  #   :t = remove leading path (basename)
+  local ruby_binary
+  for ruby_binary in $(dirname "$commands[ruby]")/*.bat(N.:r:t); do
+    alias "$ruby_binary"="$ruby_binary.bat"
+  done
+
   alias bun='nocorrect bundle.bat'
   alias bex='nocorrect bundle.bat exec'
 else
