dfcada6223429c9a5da72e681f59be195b2393cb
Alexander Groß <agross@therightstuff.de>
2016-07-08 19:52:17 +0200

    Check if SSH agent is still in a working state

--- ssh/ssh-agent.zsh
+++ ssh/ssh-agent.zsh
@@ -1,5 +1,8 @@
 if [[ "$(platform)" == "windows" ]] && [[ -z "$SSH_CONNECTION" ]]; then
-  # Unset SSH_AUTH_SOCK on Windows such that oh-my-zsh/plugins/ssh-agent does not symlink SSH_AUTH_SOCK to /tmp/ssh-agent-$USER-screen
+  # We started on Windows without coming through SSH.
+
+  # Unset SSH_AUTH_SOCK such that oh-my-zsh/plugins/ssh-agent does
+  # not symlink SSH_AUTH_SOCK to /tmp/ssh-agent-$USER-screen
   if [[ -n "$SSH_AGENT_PID" ]]; then
     verbose Unsetting user-specific system-wide export $fg[yellow]\$SSH_AGENT_PID$reset_color with $fg[yellow]$SSH_AGENT_PID$reset_color
     setx SSH_AGENT_PID "" > /dev/null
@@ -11,6 +14,33 @@ if [[ "$(platform)" == "windows" ]] && [[ -z "$SSH_CONNECTION" ]]; then
     setx SSH_AUTH_SOCK "" > /dev/null
     unset SSH_AUTH_SOCK
   fi
+
+  # Check if the local SSH agent is still in a working state.
+  local ssh_env="$HOME/.ssh/environment-$HOST"
+  if [[ -f "$ssh_env" ]]; then
+    local ssh_sock="$(sed -n 's/SSH_AUTH_SOCK=\([^;]*\);.*/\1/p' "$ssh_env")"
+
+    if [[ -n "$ssh_sock" && ! -e "$ssh_sock" ]]; then
+      # Socket points to a removed file, kill the running agent.
+      local ssh_pid="$(sed -n 's/SSH_AGENT_PID=\([^;]*\);.*/\1/p' "$ssh_env")"
+
+      if [[ -n ssh_pid ]]; then
+        verbose Killing SSH agent with PID $fg[yellow]$ssh_pid$reset_color because socket $fg[yellow]$ssh_sock$reset_color does not exist
+
+        kill -9 $ssh_pid 2> /dev/null
+      fi
+
+      unset ssh_pid
+
+      # Invalidate environment config, forcing oh-my-zsh/plugins/ssh-agent to
+      # start a new agent.
+      rm -f "$ssh_env"
+    fi
+
+    unset ssh_sock
+  fi
+
+  unset ssh_env
 fi
 
 zstyle :omz:plugins:ssh-agent agent-forwarding on
