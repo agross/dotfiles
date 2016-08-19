640ffc60e50f4966c3b7c68b0f76e2fc9c415036
Alexander Groß <agross@therightstuff.de>
2016-08-20 00:57:13 +0200

    tmux config

--- ssh/zprofile.zsh
+++ ssh/zprofile.zsh
@@ -1,9 +1,16 @@
-if [[ -n "$SSH_CONNECTION" ]] && (($+commands[screen])); then
+if [[ -n "$SSH_CONNECTION" ]]; then
   if [[ -n "$SSH_AUTH_SOCK" ]]; then
     verbose Symlinking $fg[yellow]$SSH_AUTH_SOCK$reset_color to $fg[yellow]$HOME/.ssh/ssh-agent-$USER-$HOSTNAME$reset_color
     ln -sf "$SSH_AUTH_SOCK" "$HOME/.ssh/ssh-agent-$USER-$HOSTNAME"
   fi
 
-  verbose Starting $fg[red]screen$reset_color because we\'re connected using $fg[red]ssh$reset_color
-  exec /usr/bin/screen -dRq ssh
+  if (($+commands[screen])); then
+    verbose Starting $fg[red]screen$reset_color because we\'re connected using $fg[red]ssh$reset_color
+    exec /usr/bin/screen -dRq ssh
+  fi
+
+  # if (($+commands[tmux])) && [[ -z "$TMUX" ]]; then
+  #   verbose Starting $fg[red]tmux$reset_color because we\'re connected using $fg[red]ssh$reset_color
+  #   tmux has-session -t $USER 2> /dev/null && exec tmux attach-session -t $USER || exec tmux new-session -s $USER
+  # fi
 fi
