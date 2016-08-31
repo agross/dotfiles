af21bd3dbb8c419b79a0b8697f885a8cad5856bc
Alexander Groß <agross@therightstuff.de>
2016-08-31 21:18:43 +0200

    Allow Notepad++ tab bar, fix -noPlugins switch

--- git/editor-windows.zsh
+++ git/editor-windows.zsh
@@ -9,7 +9,7 @@ if [[ -n "$SSH_CONNECTION" ]]; then
   return
 fi
 
-export GIT_EDITOR='file="$(cygpath --windows --absolute "$1")"; shift $#; /c/Tools/Notepad++/notepad++.exe -multiInst -nosession -notabbar -noPlugin "$file"'
+export GIT_EDITOR='file="$(cygpath --windows --absolute "$1")"; shift $#; /c/Tools/Notepad++/notepad++.exe -multiInst -nosession -noPlugins "$file"'
 
 # ST opens two windows when there is no instance of ST running.
 # export GIT_EDITOR='file="$(cygpath --windows --absolute "$1")"; shift $#; /c/Tools/Sublime\ Text/subl.exe --wait --new-window "$file" --'
