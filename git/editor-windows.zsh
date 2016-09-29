52bb1ae3524bf4fb4de8a03ff82f42426dc9a804
Alexander Groß <agross@therightstuff.de>
2016-09-29 21:33:53 +0200

    Switch to VS Code as the git editor

--- git/editor-windows.zsh
+++ git/editor-windows.zsh
@@ -9,10 +9,15 @@ if [[ -n "$SSH_CONNECTION" ]]; then
   return
 fi
 
-export GIT_EDITOR='file="$(cygpath --windows --absolute "$1")"; shift $#; cp "$file" "$file.git" && /c/Tools/Notepad++/notepad++.exe -multiInst -nosession -noPlugins "$file.git" && mv "$file.git" "$file"'
-
 # Visual Studio Code.
-# export GIT_EDITOR='file="$(cygpath --windows --absolute "$1")"; shift $#; /c/Tools/Code/bin/code --wait "$file"'
+export GIT_EDITOR='file="$(cygpath --windows --absolute "$1")"; shift $#; /c/Tools/Code/bin/code --new-window --wait --disable-extensions "$file"'
+
+# Notepad++ with custom syntax.
+# Files will be renamed to <name>.git before invoking Notepad++ and renamed
+# to the original file name  after Notepad++ exited. Notepad++ has a custom
+# syntax definition for *.git files:
+# https://gist.github.com/agross/f1bbe96f5b073f20639c2d93d3144ab4
+# export GIT_EDITOR='file="$(cygpath --windows --absolute "$1")"; shift $#; cp "$file" "$file.git" && /c/Tools/Notepad++/notepad++.exe -multiInst -nosession -noPlugins "$file.git" && mv "$file.git" "$file"'
 
 # ST opens two windows when there is no instance of ST running.
 # export GIT_EDITOR='file="$(cygpath --windows --absolute "$1")"; shift $#; /c/Tools/Sublime\ Text/subl.exe --wait --new-window "$file" --'
