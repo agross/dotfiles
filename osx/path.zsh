550bed210e4d33edb1338babf8d720f1e941f150
Alexander Groß <agross@therightstuff.de>
2016-11-18 11:53:32 +0100

    Mac OS X man ignores $MANPATH, so don't set it

--- osx/path.zsh
+++ osx/path.zsh
@@ -1,5 +1,1 @@
 path=($(brew --prefix)/Cellar/**/libexec/gnubin(N) $path)
-
-# Remove duplicates from global array $manpath.
-typeset -gaU manpath
-manpath=($(brew --prefix)/Cellar/**/share/man(N) $manpath)
