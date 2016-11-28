da6a4f9ea953c3adc5f5a73dc5466920779f7532
Alexander Groß <agross@therightstuff.de>
2016-11-28 23:22:23 +0100

    Ruby notifications for macOS

--- ruby/guard.rb
+++ ruby/guard.rb
@@ -1,1 +1,5 @@
-notification :gntp, host: 'localhost'
+if RbConfig::CONFIG['target_os'] =~ /mac|darwin/i
+  notification :terminal_notifier, activate: 'com.googlecode.iTerm2'
+else
+  notification :gntp, host: 'localhost'
+end
