577a9c2c3ab021201458cd726bc14c5f9a875b57
Alexander Groß <agross@therightstuff.de>
2016-11-30 22:48:11 +0100

    Support local files

---
+++ local/local.zsh
@@ -0,0 +1,11 @@
+# Glob qualifiers:
+#  .  = plain files only
+#  on = sort files by name
+
+local f
+for f in ${0%/*}/**/*(.on); do
+  [[ "$f" == "$0" ]] && continue
+
+  verbose Sourcing $fg[green]$f$reset_color
+  source "$f"
+done
