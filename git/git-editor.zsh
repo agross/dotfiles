98193bd822cd64265b5247904d5fa630e85f48eb
Alexander Groß <agross@therightstuff.de>
2016-12-04 00:00:04 +0100

    Configure git editor for windows and other platforms to VS Code

--- git/git-editor.zsh
+++ git/git-editor.zsh
@@ -1,20 +1,24 @@
-if [[ "$(platform)" != "windows" ]]; then
-  return
-fi
-
 if [[ -n "$SSH_CONNECTION" ]]; then
   # SSH and visual editors won't work.
   return
 fi
 
-# Visual Studio Code.
-export GIT_EDITOR='file="$(cygpath --windows --absolute "$1")"; shift $#; /c/Tools/Code/bin/code --new-window --wait "$file"'
+case "$(platform)" in
+  windows)
+    # Visual Studio Code.
+    export GIT_EDITOR='file="$(cygpath --windows --absolute "$1")"; shift $#; /c/Tools/Code/bin/code --new-window --wait "$file"'
+    ;;
+  *)
+    (($+commands[code])) && export GIT_EDITOR='code --new-window --wait'
+    ;;
+esac
 
 # Notepad++ with custom syntax.
 # Files will be renamed to <name>.git before invoking Notepad++ and renamed
 # to the original file name  after Notepad++ exited. Notepad++ has a custom
 # syntax definition for *.git files:
 # https://gist.github.com/agross/f1bbe96f5b073f20639c2d93d3144ab4
+#
 # export GIT_EDITOR='file="$(cygpath --windows --absolute "$1")"; shift $#; cp "$file" "$file.git" && /c/Tools/Notepad++/notepad++.exe -multiInst -nosession -noPlugins "$file.git" && mv "$file.git" "$file"'
 
 # ST opens two windows when there is no instance of ST running.
