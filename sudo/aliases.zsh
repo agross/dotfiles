15e8e44f05e28f713f6210389c28f69979c3c02d
Alexander Groß <agross@therightstuff.de>
2017-01-15 19:12:57 +0100

    SUDO_ASKPASS via keychain on macOS

--- sudo/aliases.zsh
+++ sudo/aliases.zsh
@@ -3,3 +3,8 @@
 # This allows the usage of aliases with sudo.
 # http://www.zsh.org/mla/users/2003/msg00411.html
 alias sudo='command sudo '
+
+if [[ "$(platform)" == 'mac' && -z "$SSH_CONNECTION" ]]; then
+  export SUDO_ASKPASS="${0%/*}/sudo-askpass-keychain"
+  alias sudo='command sudo -A '
+fi
