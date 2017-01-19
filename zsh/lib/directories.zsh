e3dc4d5d7723aaa2426d2c879bc8af840cfb3970
Alexander Groß <agross@therightstuff.de>
2017-01-19 23:47:43 +0100

    Fix prompt cwd being incorrect when cwd has been moved to the macOS trash
    If you move the current working directory to the trash, and moved one directory
    level up, the prompt would display the same current working directory despite
    now being at $USER/.Trash/<directory>.
    
    Example:
    
    [~] $ mkdir test && cd test
    [~/test] $
    [~/test] $ cd ..
    [~] $ # <-- This is incorrect.
    [~] $ echo $PWD
    /Users/agross <-- Also incorrect.
    [~] $ pwd
    /Users/agross/.Trash <-- This is correct and also what the prompt will display
                             with this patch applied (e.g. the short form of ~/.Trash)

--- zsh/lib/directories.zsh
+++
@@ -1,6 +0,0 @@
-# Any parameter that is set to the absolute name of a directory immediately
-# becomes a name for that directory, that will be used by the `%~' and
-# related prompt sequences, and will be available when completion is performed
-# on a word starting with `~'. (Otherwise, the parameter must be used in the
-# form `~param' first.)
-setopt auto_name_dirs
