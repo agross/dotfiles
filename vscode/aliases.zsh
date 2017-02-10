e5267629d468afb2d08b5661ecfd3d57c54e4f3b
Alexander Groß <agross@therightstuff.de>
2017-02-10 20:20:30 +0100

    vscode: alias, remove unnecessary file check

--- vscode/aliases.zsh
+++ vscode/aliases.zsh
@@ -1,3 +1,3 @@
-if [[ "$OSTYPE" == 'cygwin' && -f '/C/Tools/Code/bin/code' && -x '/C/Tools/Code/bin/code' ]]; then
+if [[ "$OSTYPE" == 'cygwin' && -x '/C/Tools/Code/bin/code' ]]; then
   alias code='/C/Tools/Code/bin/code'
 fi
