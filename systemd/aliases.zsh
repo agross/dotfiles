e6bcea9f18fe1e5fc362ec5522c5097279d8562e
Alexander Groß <agross@therightstuff.de>
2017-02-16 18:08:26 +0100

    systemd: added alias for journalctl

--- systemd/aliases.zsh
+++ systemd/aliases.zsh
@@ -4,4 +4,5 @@ verbose Setting up $fg[red]systemd$reset_color aliases
 
 alias sc=systemctl
 alias scf='systemctl --failed'
-alias jf='journalctl --since="1 hour ago" --unit'
+alias ju='journalctl --since="1 hour ago" --unit'
+alias jf='journalctl --follow --unit'
