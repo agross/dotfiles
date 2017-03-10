0c3e253e7f193ad782431c9d57240e257fc29182
Alexander Groß <agross@therightstuff.de>
2017-03-10 10:03:27 +0100

    git: define global alias GU to specify upstream branch for the current branch (use anywhere on the command line)

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,1 +1,6 @@
+(($+commands[etckeeper])) || return 0
+
 alias g=git
+# Mnemonic: Git upstream.
+# https://git-scm.com/docs/gitrevisions#gitrevisions-emltbranchnamegtupstreamemegemmasterupstreamememuem
+alias -g GU='HEAD@{upstream}'
