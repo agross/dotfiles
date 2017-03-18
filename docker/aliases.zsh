5d978f47308c3f2af2ca14ee76064d3d1f2445c2
Alexander Groß <agross@therightstuff.de>
2017-03-18 21:47:44 +0100

    docker: add alias to remove all dangling images

--- docker/aliases.zsh
+++ docker/aliases.zsh
@@ -4,3 +4,4 @@ verbose Setting up $fg[red]docker$reset_color aliases
 
 alias dstats='docker stats $(docker ps --format={{.Names}})'
 alias dpull='docker images --format="{{.Repository}}:{{.Tag}}" | command grep -v ":<none>" | xargs --no-run-if-empty --max-lines=1 docker pull'
+alias dclean='docker images --quiet --filter=dangling=true | xargs --no-run-if-empty docker rmi'
