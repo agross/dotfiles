3429cd095a2d2bd81b9665c2be956d4b0baf561d
Alexander Groß <agross@therightstuff.de>
2017-03-18 21:35:48 +0100

    docker: add alias to pull all tagged images

--- docker/aliases.zsh
+++ docker/aliases.zsh
@@ -3,3 +3,4 @@
 verbose Setting up $fg[red]docker$reset_color aliases
 
 alias dstats='docker stats $(docker ps --format={{.Names}})'
+alias dpull='docker images --format="{{.Repository}}:{{.Tag}}" | command grep -v ":<none>" | xargs --no-run-if-empty --max-lines=1 docker pull'
