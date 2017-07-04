973463e9ee99a74321a92bbe97e77164af4d2a67
Alexander Groß <agross@therightstuff.de>
2017-07-04 23:08:35 +0200

    paket: add completion

--- paket/aliases.zsh
+++ paket/aliases.zsh
@@ -3,3 +3,6 @@ if [[ "$OS" != Windows* ]]; then
 else
   alias paket='./.paket/paket.exe'
 fi
+
+# Prefer executable in bin/ for Paket development.
+zstyle ':completion::complete:paket:*' paket-executable './bin/paket.exe' './.paket/paket.exe'
