fd33df071d9c2b748a17062d80e716442fb5bc6d
Alexander Groß <agross@therightstuff.de>
2017-07-24 18:07:25 +0200

    aiccu: remove after project was discontinued

--- aiccu/path.zsh
+++
@@ -1,6 +0,0 @@
-[[ "$OSTYPE" == darwin* ]] || return 0
-
-local bin_path="${0%/*}/bin"
-path=($bin_path $path)
-
-verbose Prepending $fg[yellow]$bin_path$reset_color to $fg[yellow]\$PATH$reset_color
