8770470493b1065946c568b0b9af32aa12ffe6c8
Alexander Groß <agross@therightstuff.de>
2017-08-10 21:11:38 +0200

    docker: enable option stacking for better docker exec completion

---
+++ docker/completion.zsh
@@ -0,0 +1,3 @@
+# https://github.com/moby/moby/commit/402caa94d23ea3ad47f814fc1414a93c5c8e7e58
+zstyle ':completion:*:*:docker:*' option-stacking yes
+zstyle ':completion:*:*:docker-*:*' option-stacking yes
