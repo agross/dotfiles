1d97abf9f64b5dd3da52bf3e0a4c278e2c764052
Alexander Groß <agross@therightstuff.de>
2017-09-03 21:12:34 +0200

    zsh: support comments in interactive command lines

--- zsh/lib/prompt.zsh
+++ zsh/lib/prompt.zsh
@@ -1,11 +1,18 @@
-# If set, parameter expansion, command substitution and arithmetic expansion are performed in prompts. Substitutions within prompts do not affect the command status.
+# Support # comments in command lines.
+setopt interactivecomments
+
+# If set, parameter expansion, command substitution and arithmetic expansion are
+# performed in prompts. Substitutions within prompts do not affect the command
+# status.
 setopt prompt_subst
 
 # Print a carriage return just before printing a prompt in the line editor.
 setopt PROMPT_CR
 
-# Attempt to preserve a partial line (i.e. a line that did not end with a newline) that would otherwise be covered up by the command prompt due to the PROMPT_CR option.
-# Typically caused by null-terminating characters, e.g. find /tmp -print0
+# Attempt to preserve a partial line (i.e. a line that did not end with a
+# newline) that would otherwise be covered up by the command prompt due to the
+# PROMPT_CR option. Typically caused by null-terminating characters, e.g. find
+# /tmp -print0
 setopt PROMPT_SP
 
 # Load the prompt theme system.
