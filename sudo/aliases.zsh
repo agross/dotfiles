81f4ec4b7c2edc7c3cb1783278fa70a44d65c757
Alexander Groß <agross@therightstuff.de>
2017-09-18 22:55:28 +0200

    sudo: prefix command not on Synology, does not seem to work

--- sudo/aliases.zsh
+++ sudo/aliases.zsh
@@ -2,9 +2,11 @@
 
 # This allows the usage of aliases with sudo.
 # http://www.zsh.org/mla/users/2003/msg00411.html
-alias sudo='command sudo '
+if [[ ! -f /proc/syno_cpu_arch ]]; then
+  alias sudo='command sudo '
+fi
 
-if [[ "$(platform)" == 'mac' && -z "$SSH_CONNECTION" ]]; then
+if [[ "$OSTYPE" == darwin* && -z "$SSH_CONNECTION" ]]; then
   export SUDO_ASKPASS="${0%/*}/sudo-askpass-keychain"
   alias sudo='command sudo -A '
 fi
