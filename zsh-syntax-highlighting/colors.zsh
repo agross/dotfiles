bf27570ac3ce4f732f95146f6b60b15f73d3100a
Alexander Groß <agross@therightstuff.de>
2017-10-16 00:09:37 +0200

    zsh: use faster syntax highlight, also on Windows

--- zsh-syntax-highlighting/colors.zsh
+++ zsh-syntax-highlighting/colors.zsh
@@ -1,15 +1,21 @@
-typeset -gA ZSH_HIGHLIGHT_STYLES
-ZSH_HIGHLIGHT_STYLES[assign]='fg=blue,bold'
-ZSH_HIGHLIGHT_STYLES[commandseparator]='fg=magenta,bold'
-ZSH_HIGHLIGHT_STYLES[globbing]='fg=cyan,bold'
-ZSH_HIGHLIGHT_STYLES[precommand]='fg=green,bold'
-ZSH_HIGHLIGHT_STYLES[redirection]='fg=magenta,bold'
-ZSH_HIGHLIGHT_STYLES[reserved-word]='fg=magenta,bold'
-ZSH_HIGHLIGHT_STYLES[single-quoted-argument]='fg=yellow,bold'
-ZSH_HIGHLIGHT_STYLES[double-quoted-argument]='fg=yellow,bold'
-ZSH_HIGHLIGHT_STYLES[suffix-alias]='fg=green,bold'
+# Get all styles with:
+# print -l ${(ko)FAST_HIGHLIGHT_STYLES}
+typeset -gA FAST_HIGHLIGHT_STYLES
+FAST_HIGHLIGHT_STYLES[assign]='fg=blue,bold'
+FAST_HIGHLIGHT_STYLES[commandseparator]='fg=magenta,bold'
+FAST_HIGHLIGHT_STYLES[double-quoted-argument]='fg=yellow,bold'
+FAST_HIGHLIGHT_STYLES[globbing]='fg=cyan,bold'
+FAST_HIGHLIGHT_STYLES[path]=underline
+FAST_HIGHLIGHT_STYLES[precommand]='fg=green,bold'
+FAST_HIGHLIGHT_STYLES[redirection]='fg=magenta,bold'
+FAST_HIGHLIGHT_STYLES[reserved-word]='fg=magenta,bold'
+FAST_HIGHLIGHT_STYLES[single-quoted-argument]='fg=yellow,bold'
+FAST_HIGHLIGHT_STYLES[suffix-alias]='fg=green,bold'
+FAST_HIGHLIGHT_STYLES[variable]='fg=green,bold'
 
+# Do we have 256 color support?
 if [[ "$(echotc Co)" == '256' ]]; then
-  ZSH_HIGHLIGHT_STYLES[assign]='fg=39' # Blue.
-  ZSH_HIGHLIGHT_STYLES[reserved-word]='fg=200' # Magenta.
+  FAST_HIGHLIGHT_STYLES[assign]='fg=39' # Blue.
+  FAST_HIGHLIGHT_STYLES[reserved-word]='fg=200' # Magenta.
+  FAST_HIGHLIGHT_STYLES[variable]='fg=112' # Green
 fi
