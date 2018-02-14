2e5eec1bef8b4edd3a9526471cdf8c41f5f135cc
Alexander Groß <agross@therightstuff.de>
2018-02-14 18:38:33 +0100

    docker: add drestart to restart all systemd services running outdated docker containers

--- docker/aliases.zsh
+++ docker/aliases.zsh
@@ -5,6 +5,7 @@ verbose Setting up $fg[red]docker$reset_color aliases
 alias dclean='docker images --quiet --filter=dangling=true | xargs --no-run-if-empty docker rmi'
 alias dhealth='docker inspect --format="{{json .State.Health}}"'
 alias dpull='docker images --format="{{.Repository}}:{{.Tag}}" | command grep -v ":<none>" | xargs --no-run-if-empty --max-lines=1 docker pull'
+alias drestart="docker ps --no-trunc --format='table {{.Image}} {{.Names}}' | command grep --extended-regexp '^sha256:[0-9a-f]{64}' | cut -d ' ' -f2 | cut -d _ -f 1 | xargs --no-run-if-empty --verbose sudo systemctl restart"
 alias dstats='docker stats $(docker ps --format={{.Names}})'
 
 if [[ $OSTYPE == darwin* ]]; then
