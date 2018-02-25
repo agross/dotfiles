814ca5387f06322ca542a133d41aa4e929bbe0ff
Alexander Groß <agross@therightstuff.de>
2018-02-25 23:42:09 +0100

    wsl: remove Windows and Cygwin binaries from PATH

--- wsl/path.zsh
+++ wsl/path.zsh
@@ -2,4 +2,10 @@ if [[ -f /proc/version ]] && \
    grep --quiet Microsoft /proc/version; then
   # Remove all PATHs starting with /mnt/ to get rid of Windows binaries in PATH.
   path=(${path##/mnt/*})
+  # Remove all Dropbox PATHs.
+  path=(${path##/d/Dropbox/*})
+  # Remove all Cygwin PATHs.
+  path=(${path##/c/Cygwin/*})
+  # Remove npm from PATH.
+  path=(${path##/c/Users/${USER}/AppData/Roaming/npm})
 fi
