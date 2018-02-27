2e7e161092c3995d475d840583c0c60369f9ec27
Alexander Groß <agross@therightstuff.de>
2018-02-27 11:05:50 +0100

    zsh: new alias rmrf for rm -rf

--- zsh/lib/aliases.zsh
+++ zsh/lib/aliases.zsh
@@ -11,6 +11,7 @@ alias -g E2O='2>&1'
 alias md='mkdir --parents'
 alias grep='grep --color=auto --ignore-case --binary-files=without-match --line-number --initial-tab'
 alias ls='ls --color=auto -l --all --human-readable --group-directories'
+alias rmrf='rm --recursive --force'
 
 # Also done by Ctrl + L.
 alias cls='echo -ne "\033c"'
