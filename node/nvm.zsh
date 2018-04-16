83f741e6c0ef5e1ee8ac10b9bb3af3da64181e37
Alexander Groß <agross@therightstuff.de>
2018-04-16 19:22:18 +0200

    node: support NVM being installed in $HOME

--- node/nvm.zsh
+++ node/nvm.zsh
@@ -1,6 +1,14 @@
-[[ -f /usr/local/opt/nvm/nvm.sh ]] || return 0
+local -a nvm
+nvm=(/usr/local/opt/nvm/nvm.sh $HOME/.nvm/nvm.sh)
+
+local candidate match
+for candidate in $nvm; do
+  [[ -f $candidate ]] && match=$candidate && break
+done
+
+[[ -n $match ]] || return 0
 
 export NVM_DIR="$HOME/.nvm"
 [[ ! -d "$NVM_DIR" ]] && mkdir "$NVM_DIR"
 
-source "/usr/local/opt/nvm/nvm.sh"
+source "$match"
