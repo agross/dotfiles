c66d18ced7ed1cbb665824a7914bd1cfa9345556
Alexander Groß <agross@therightstuff.de>
2018-12-20 11:42:55 +0100

    docker: improve dpull and dclean

--- docker/aliases.zsh
+++ docker/aliases.zsh
@@ -1,9 +1,8 @@
 (($+commands[docker])) || return 0
 
-verbose Setting up $fg[red]docker$reset_color aliases
-
-alias dclean='docker image prune --all --force'
 
 if [[ $OSTYPE == darwin* ]]; then
+  verbose Setting up $fg[red]docker$reset_color aliases
+
   alias moby='docker run --rm -it --privileged --pid=host walkerlee/nsenter -t 1 -m -u -i -n sh'
 fi
