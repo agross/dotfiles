175a0755d2815c56da7f2bb3559052a41a9303a6
Alexander Groß <agross@therightstuff.de>
2019-09-15 01:13:12 +0200

    cygwin: make open an executable and support Total Commander
    open -t opens the passed path in Total Commander
    open without -t delegates to cygstart as before

--- cygwin/aliases.zsh
+++ cygwin/aliases.zsh
@@ -7,4 +7,3 @@ alias rsd='rm -f *.exe.stackdump'
 
 verbose Setting up $fg[red]cygstart$reset_color aliases
 alias sudo='cygstart --action=runas'
-alias open='cygstart'
---
+++ cygwin/path.zsh
@@ -0,0 +1,8 @@
+# shellcheck disable=SC2148
+
+[[ "$(platform)" == 'windows' ]] || return 0
+
+local bin_path="${0%/*}/bin"
+path=($bin_path $path)
+
+verbose Prepending $fg[yellow]$bin_path$reset_color to $fg[yellow]\$PATH$reset_color
