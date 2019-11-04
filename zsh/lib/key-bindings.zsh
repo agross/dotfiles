be2f5cf67d70cbd7a8560c54b2ba4660b8d8fcd9
Alexander Groß <agross@therightstuff.de>
2019-11-05 00:36:03 +0100

    zsh: edit command line with ESC, e

--- zsh/lib/key-bindings.zsh
+++ zsh/lib/key-bindings.zsh
@@ -67,3 +67,10 @@ bindkey '\e1'     insert-last-command-output # ESC, 1
 autoload -Uz transpose-words-match
 zstyle ':zle:transpose-words' word-style shell
 zle -N transpose-words transpose-words-match
+
+# Edit the command line with Ctrl + X, Ctrl +E or Esc, e.
+# In addition, you may use `fc` to fix the last command executed.
+autoload -Uz edit-command-line
+zle -N edit-command-line
+bindkey '^X^E' edit-command-line
+bindkey '\ee' edit-command-line
