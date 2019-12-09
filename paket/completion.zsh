f07036bef23b4d412fa93a4cc762e01be1d0fb7b
Alexander Groß <agross@therightstuff.de>
2019-12-09 02:40:00 +0100

    paket: convert alias to binary

--- paket/completion.zsh
+++ paket/completion.zsh
@@ -1,8 +1,2 @@
-if [[ "$OS" != Windows* ]]; then
-  alias paket='mono ./.paket/paket.exe'
-else
-  alias paket='./.paket/paket.exe'
-fi
-
 # Prefer executable in bin/ for Paket development.
 zstyle ':completion::complete:paket:*' paket-executable './bin/paket.exe' './.paket/paket.exe'
