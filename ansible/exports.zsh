2283b32e291513aafe66f36cb7a1790d8f45447a
Alexander Groß <agross@therightstuff.de>
2021-02-17 13:06:53 +0100

    ansible: add keychain client to retrieve vault IDs

---
+++ ansible/exports.zsh
@@ -0,0 +1,9 @@
+(($+commands[ansible])) || return 0
+
+VAULT=${0%/*}/bin/vault-keychain-client
+
+export ANSIBLE_VAULT_ID_MATCH=true
+export ANSIBLE_VAULT_IDENTITY_LIST="agross@$VAULT,
+heco@$VAULT,
+kessler@$VAULT,
+gw@$VAULT"
