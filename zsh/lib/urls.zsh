50847a6f24e079da6fb1c8568fdfe79307fec1e6
Alexander Groß <agross@therightstuff.de>
2021-08-04 20:04:25 +0200

    zsh: restore url-quote-magic on zsh 5.1
    https://github.com/zsh-users/zsh/blob/master/Functions/Zle/url-quote-magic

--- zsh/lib/urls.zsh
+++ zsh/lib/urls.zsh
@@ -1,3 +1,7 @@
-# The url-quote-magic line editing plugin automatically quotes metacharacters like question marks, quotes and ampersands while you type or paste them.
+# The url-quote-magic line editing plugin automatically quotes metacharacters
+# like question marks, quotes and ampersands while you type or paste them.
 autoload -U url-quote-magic
 zle -N self-insert url-quote-magic
+
+autoload -Uz bracketed-paste-magic
+zle -N bracketed-paste bracketed-paste-magic
