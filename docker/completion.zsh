9ba339fd5c86fa33b62b830ea6277d1c7458006c
Alexander Groß <agross@therightstuff.de>
2021-12-31 00:32:24 +0100

    docker: install dive

--- docker/completion.zsh
+++ docker/completion.zsh
@@ -1,6 +1,10 @@
 # Use docker completion functions for commands in bin.
 if (( $+functions[_docker] )); then
   compdef _docker dhealth=_docker_complete_containers
+
+  if (( $+commands[dive] )); then
+    compdef __docker_complete_repositories_with_tags dive
+  fi
 fi
 
 # https://github.com/moby/moby/commit/402caa94d23ea3ad47f814fc1414a93c5c8e7e58
