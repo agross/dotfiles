dcb352aa19a944ca1f6b3970c50458ab9ced4903
Alexander Groß <agross@therightstuff.de>
2022-08-02 13:18:50 +0200

    ssh/screen: update SSH_AUTH_SOCK symlink in home directory with the current connection's socket

--- ssh/zprofile.zsh
+++ ssh/zprofile.zsh
@@ -1,18 +1,23 @@
-if [[ -n $SSH_CONNECTION ]]; then
-  [[ -v NO_TERMINAL_MULTIPLEXER ]] && return 0
+[[ -z $SSH_CONNECTION ]] && return 0
+[[ -v NO_TERMINAL_MULTIPLEXER ]] && return 0
 
-  if (($+commands[screen])); then
-    verbose Starting $fg[red]screen$reset_color because we\'re connected \
-            using $fg[red]ssh$reset_color
-
-    # Synology does not support xterm-256color.
-    [[ -f /proc/syno_cpu_arch ]] && export TERM=xterm
-
-    exec screen -dRq ssh
+if (($+commands[screen])); then
+  if [[ -S $SSH_AUTH_SOCK && ! -h $SSH_AUTH_SOCK ]]; then
+    verbose Symlinking $fg[yellow]$SSH_AUTH_SOCK$reset_color to \
+            $fg[yellow]~/.ssh/ssh_auth_sock$reset_color
+    ln -sf -- $SSH_AUTH_SOCK ~/.ssh/ssh_auth_sock
   fi
 
-  # if (($+commands[tmux])) && [[ -z "$TMUX" ]]; then
-  #   verbose Starting $fg[red]tmux$reset_color because we\'re connected using $fg[red]ssh$reset_color
-  #   tmux has-session -t $USER 2> /dev/null && exec tmux attach-session -t $USER || exec tmux new-session -s $USER
-  # fi
+  verbose Starting $fg[red]screen$reset_color because we\'re connected \
+          using $fg[red]ssh$reset_color
+
+  # Synology does not support xterm-256color.
+  [[ -f /proc/syno_cpu_arch ]] && export TERM=xterm
+
+  exec screen -dRq ssh
 fi
+
+# if (($+commands[tmux])) && [[ -z "$TMUX" ]]; then
+#   verbose Starting $fg[red]tmux$reset_color because we\'re connected using $fg[red]ssh$reset_color
+#   tmux has-session -t $USER 2> /dev/null && exec tmux attach-session -t $USER || exec tmux new-session -s $USER
+# fi
