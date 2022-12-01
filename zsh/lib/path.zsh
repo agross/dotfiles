8b833add01137b14a6034a574824091c3308a8ea
Alexander Groß <agross@therightstuff.de>
2022-12-01 15:31:01 +0100

    zsh: add sbin to PATH on Debian

--- zsh/lib/path.zsh
+++ zsh/lib/path.zsh
@@ -3,5 +3,9 @@ typeset -gaU path
 
 path=($HOME/bin(N) $HOME/bin/*/(/N) $path)
 
+if [[ -f /etc/debian_version ]]; then
+  path=($path /usr/local/sbin /usr/sbin /sbin)
+fi
+
 # ZSH sets $PATH automatically from $path.
 verbose Setting $fg[yellow]\$PATH$reset_color to $fg[yellow]$path$reset_color
