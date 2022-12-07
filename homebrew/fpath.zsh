59bc3f446c4b214f1270d678b2303aa561de5103
Alexander Groß <agross@therightstuff.de>
2022-12-07 13:23:20 +0100

    homebrew: use homebrew-installed zsh completions

--- homebrew/fpath.zsh
+++ homebrew/fpath.zsh
@@ -2,5 +2,8 @@ if [[ ! "$OSTYPE" == darwin* ]]; then
   return
 fi
 
+# Add homebrew-installed zsh completions.
+fpath=($HOMEBREW_PREFIX/share/zsh/site-functions $fpath)
+
 # Make sure homebrew zsh functions take precedence.
 fpath=($HOMEBREW_CELLAR/zsh/$ZSH_VERSION/**/functions(/N) $fpath)
