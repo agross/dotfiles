fa9ef87fb9555d7441388d3cc85a59cbfca706af
Alexander Groß <agross@therightstuff.de>
2023-08-10 12:11:34 +0200

    ansible: add vault ID for co-IT

--- ansible/exports.zsh
+++ ansible/exports.zsh
@@ -4,6 +4,7 @@ VAULT=${0%/*}/bin/vault-keychain-client
 
 export ANSIBLE_VAULT_ID_MATCH=true
 export ANSIBLE_VAULT_IDENTITY_LIST="agross@$VAULT,
+co-it@$VAULT,
 heco@$VAULT,
 kessler@$VAULT,
 gw@$VAULT"
