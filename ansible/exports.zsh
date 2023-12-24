0dbc66fb3ba3cfbb7aa613ce29f5476e5789b675
Alexander Groß <agross@therightstuff.de>
2023-12-24 11:25:51 +0100

    ansible: add new vault identity

--- ansible/exports.zsh
+++ ansible/exports.zsh
@@ -5,6 +5,7 @@ VAULT=${0%/*}/bin/vault-keychain-client
 
 export ANSIBLE_VAULT_ID_MATCH=true
 export ANSIBLE_VAULT_IDENTITY_LIST="agross@$VAULT,
+agross2@$VAULT,
 co-it@$VAULT,
 heco@$VAULT,
 kessler@$VAULT,
