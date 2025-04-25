a56bbaf3cb514a0c3b33b78481a0074cd349998d
Alexander Groß <agross@therightstuff.de>
2025-04-25 15:53:06 +0200

    systemd: make jf a function

--- systemd/aliases.zsh
+++ systemd/aliases.zsh
@@ -4,16 +4,3 @@ verbose Setting up $fg[red]systemd$reset_color aliases
 
 alias sc=systemctl
 alias scf='systemctl --failed'
-alias jf='_() {
-  local arg
-  local -a args
-  for arg in $@; do
-    if [[ $arg != -* ]] then
-      args+=(--unit $arg)
-    else
-      args+=($arg)
-    fi
-  done
-  journalctl --follow ${args[@]}
-}
-_'
