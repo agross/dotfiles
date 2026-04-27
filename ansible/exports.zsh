2480484b34482683d60888a5923254c7c318bbd8
Alexander Groß <agross@therightstuff.de>
2026-04-27 17:05:19 +0200

    ansible: drop vault IDs that are no longer used

--- ansible/exports.zsh
+++ ansible/exports.zsh
@@ -4,9 +4,7 @@
 VAULT=${0%/*}/bin/vault-keychain-client
 
 export ANSIBLE_VAULT_ID_MATCH=true
-export ANSIBLE_VAULT_IDENTITY_LIST="agross@$VAULT,
-agross2@$VAULT,
+export ANSIBLE_VAULT_IDENTITY_LIST="agross2@$VAULT,
 co-it@$VAULT,
 heco@$VAULT,
-kessler@$VAULT,
 gw@$VAULT"
